7cc89f73091aac61342cfd54c229c3d1ad54e94e
MateYa05 <mateakarimovic15@gmail.com>
2025-06-30 16:44:49 +0200

    main.c

--- main.c
+++ main.c
@@ -3,30 +3,30 @@
 #include <stdlib.h>
 #include "serije.h"
 #include "datoteka.h"
+#include <string.h>
 
 typedef enum {
     OPCIJA_DODAJ_SERIJU = 1,
     OPCIJA_PRETRAGA,
+    OPCIJA_OBRISI_SERIJU,  
     OPCIJA_IZLAZ
 } IzbornikOpcije;
 
 int main() {
     int opcija;
     while (1) {
-        printf("\n1. Dodaj seriju\n2. Pretraga\n3. Izlaz\nOdabir: ");
+        printf("\n1. Dodaj seriju\n2. Pretraga\n3. Obrisi seriju\n4. Izlaz\nOdabir: ");
         scanf("%d", &opcija);
 
-        if (opcija == 1) {
+        if (opcija == OPCIJA_DODAJ_SERIJU) {
             Serija* s = unesiSeriju();
             if (s) {
                 spremiSeriju(s);
                 free(s->naziv);
                 free(s);
-                s->naziv = NULL;
-                s = NULL;
             }
         }
-        else if (opcija == 2) {
+        else if (opcija == OPCIJA_PRETRAGA) {
             int pod;
             printf("\n1. Po imenu\n2. Po zanru\nOdabir: ");
             scanf("%d", &pod);
@@ -43,7 +43,15 @@ int main() {
                 traziPoZanru(z);
             }
         }
-        else if (opcija == 3) {
+        else if (opcija == OPCIJA_OBRISI_SERIJU) {
+            char naziv[100];
+            getchar();  
+            printf("Unesi naziv serije koju želiš obrisati: ");
+            fgets(naziv, sizeof(naziv), stdin);
+            naziv[strcspn(naziv, "\n")] = 0;
+            obrisiSeriju(naziv);
+        }
+        else if (opcija == OPCIJA_IZLAZ) {
             break;
         }
         else {
